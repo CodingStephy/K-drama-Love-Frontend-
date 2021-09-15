a1140fee267c25fa9291a90a97c023c6ab54e2f7
redpumpkin21 <christopher.johnson2108@gmail.com>
2021-09-15 03:11:16 -0400

    carousel implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {YouTubePlayerModule} from '@angular/youtube-player';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { CarouselComponent } from './carousel/carousel.component';
+import { TrendingComponent } from './trending/trending.component';
+import { RecommendedComponent } from './recommended/recommended.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarouselComponent,
+    TrendingComponent,
+    RecommendedComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    YouTubePlayerModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
